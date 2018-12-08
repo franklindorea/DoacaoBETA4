8ca4ab915aece8f17a0d62b25dedbfc28f636d14
franklindorea <franklindorea@gmail.com>
2018-12-08 14:11:39 -0300

    Initial commit inicio do projeto

---
+++ app/src/main/java/com/doaacao/doacaobeta4/fazerdoaacaoActivity.java
@@ -0,0 +1,16 @@
+package com.doaacao.doacaobeta4;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+//classe para nova postagem de doaçao
+public class fazerdoaacaoActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fazerdoaacao);
+
+    }
+}
